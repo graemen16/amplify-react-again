fa55ab88f64a9bb85cb59cc456a1146a1e7b243c
Graeme Nelson <graemenelson@annes-mbp-2.home>
2023-02-26 17:14:14 +0000

    now a notes app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql24fc81c3": {
       "AppClientID": "string",
